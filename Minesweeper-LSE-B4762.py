ebddf6182650939179300550ce17d8891a2702bb
Natanael Laska <colagubb3n.hotmail.com>
2021-04-22 15:41:11 +0200

    started with flagging system

--- Minesweeper-LSE-B4762.py
+++ Minesweeper-LSE-B4762.py
@@ -11,7 +11,7 @@ def revealFirstTime(field1,unlock,field):
             return "*"
         return field1[unlock]
     for k in range(8,11):
-        if unlock+k<72:
+        if unlock+k<82:
             if field1[unlock+k]=="M":
                 c+=1
     if field1[unlock+1]=="M":
@@ -35,7 +35,7 @@ def reveal(field1,unlock,field,firstTimeCheck):
     if field1[unlock]!="*":
         return field1[unlock]
     for k in range(8,11):
-        if unlock+k<72:
+        if unlock+k<82:
             if field1[unlock+k]=="M":
                 c+=1
     if field1[unlock+1]=="M":
@@ -54,20 +54,20 @@ def reveal(field1,unlock,field,firstTimeCheck):
         if c>0:
             for k in range(8,11):
                 a=unlock+k
-                if a<72 and a>0:
+                if a>81 or a<0:
                     field[unlock+k]=revealFirstTime(field1,a,field)
             k=1
             a=unlock+k
-            if a<72 and a>0:
+            if a>81 or a<0:
                 field[unlock+k]=revealFirstTime(field1,a,field)
             k=-1
             a=unlock+k
-            if a<72 and a>0:
+            if a>81 or a<0:
                 field[unlock+k]=revealFirstTime(field1,a,field)  
             for k in range(-8,-11,-1):
                 c=0
                 a=unlock+k
-                if a<72 and a>0:
+                if a>81 or a<0:
                     c=revealFirstTime(field1,a,field)
                     field[unlock+k]=c
     counter=-1
@@ -78,21 +78,21 @@ def reveal(field1,unlock,field,firstTimeCheck):
             if i ==0:
                 for k in range(8,11):
                     a=counter+k
-                    if a<72 and a>0:
+                    if a<82 or a>0:
                         c=revealFirstTime(field1,a,field)
                         if c==0:
                             field[counter+k]="0"                
                             field[counter+k]=c  #ÄNDRA ALLT DET HÄR TILL DET HÄR :)                    
                 k=1
                 a=counter+k
-                if a<72 and a>0:
+                if a<82 or a>0:
                     c=revealFirstTime(field1,a,field)
                     if c==0:
                         c="0"          
                     field[counter+k]=c 
                 k=-1
                 a=counter+k
-                if a<72 and a>0:
+                if a<82 or a>0:
                     c=revealFirstTime(field1,a,field) 
                     if c==0:
                         c="0"              
@@ -100,7 +100,7 @@ def reveal(field1,unlock,field,firstTimeCheck):
                 for k in range(-8,-11,-1):
                     c=0
                     a=counter+k
-                    if a<72 and a>0:
+                    if a<82 or a>0:
                         c=revealFirstTime(field1,a,field)
                         field[counter+k]=c
                         if c==0:
@@ -116,59 +116,28 @@ def unlock(coordinates,field1,field,firstTimeCheck):
     field=reveal(field1,unlock,field,firstTimeCheck)
     return field
 
-def answer1(field):
-    coordinates="00000"
-    while len(coordinates)!=2 or coordinates[0]!="f" or coordinates[0]!="F":
-        coordinates=input("Type in the X and Y coordinates you want to unlock in the following format\n 'x,y', if you wanna mark a flag type "F" and then the coordinates.\n :")
-    while coordinates=="F" or coordinates=="f":
-        coordinates=input("Type in the coordinates in the following format 'x,y' : ")
-        while len(coordinates)!=2:
-            coordinates=input("Make sure you are typing this format'x,y': ")
-        while not (coordinates[0].isdigit() and coordiantes[2].isdigit()):
-            coordinates=input("Make sure you are typing this format 'x,y': ")
-        if coordinates[0] == 0 or coordinates[2] == 0:
-            coordinates=input("No 0's are allowed. Please type again. 'x,y': ")
-        x=int(coordinates[0])
-        y=int(coordinates[2])
-        unlock=(x*y)-((y-1)*x)
-        if y < 8:
-            unlock=((8-y)*(9))+(unlock)
-        field[unlock]="F"
-        coordinates=input("Type in the X and Y coordinates you want to unlock in the following format\n 'x,y', if you wanna mark a flag type "F" and then the coordinates.\n :")
-        while len(coordinates)!=2 or coordinates[0]!="f" or coordinates[0]!="F":
-            coordinates=input("Type in the X and Y coordinates you want to unlock in the following format\n 'x,y', if you wanna mark a flag type "F" and then the coordinates.\n :")
-    if len(coordinates)==2:
-        coordinates=input("Type in the coordinates in the following format 'x,y' : ")
-        while len(coordinates) !=2:
-            coordinates=input("Make sure you are typing this format'x,y': ")
-        while not (coordinates[0].isdigit() and coordiantes[2].isdigit()):
-            coordinates=input("Make sure you are typing this format 'x,y': ")
-        if coordinates[0] == 0 or coordinates[2] == 0:
-            coordinates=input("No 0's are allowed. Please type again. 'x,y': ")
-    return coordinates
-
 def mineSweeper():
     field=["(y)"'\n'
-           " 8 ","*","*","*","*","*","*","*","*",'\n'
-           " 7 ","*","*","*","*","*","*","*","*",'\n'
-           " 6 ","*","*","*","*","*","*","*","*",'\n'
-           " 5 ","*","*","*","*","*","*","*","*",'\n'
-           " 4 ","*","*","*","*","*","*","*","*",'\n'
-           " 3 ","*","*","*","*","*","*","*","*",'\n'
-           " 2 ","*","*","*","*","*","*","*","*",'\n'
-           " 1 ","*","*","*","*","*","*","*","*",'\n'
-           "   ","1","2","3","4","5","6","7","8","(x)"]
+           " 8","*","*","*","*","*","*","*","*",'\n'
+           " 7","*","*","*","*","*","*","*","*",'\n'
+           " 6","*","*","*","*","*","*","*","*",'\n'
+           " 5","*","*","*","*","*","*","*","*",'\n'
+           " 4","*","*","*","*","*","*","*","*",'\n'
+           " 3","*","*","*","*","*","*","*","*",'\n'
+           " 2","*","*","*","*","*","*","*","*",'\n'
+           " 1","*","*","*","*","*","*","*","*",'\n'
+           "  ","1","2","3","4","5","6","7","8","(x)"]
     print(*field)
     field1=["(y)"'\n'
-           " 8 ","*","*","*","*","*","*","*","*",'\n'
-           " 7 ","*","*","*","*","*","*","*","*",'\n'
-           " 6 ","*","*","*","*","*","*","*","*",'\n'
-           " 5 ","*","*","*","*","*","*","*","*",'\n'
-           " 4 ","*","*","*","*","*","*","*","*",'\n'
-           " 3 ","*","*","*","*","*","*","*","*",'\n'
-           " 2 ","*","*","*","*","*","*","*","*",'\n'
-           " 1 ","*","*","*","*","*","*","*","*",'\n'
-           "   ","1","2","3","4","5","6","7","8","(x)"]
+           " 8","*","*","*","*","*","*","*","*",'\n'
+           " 7","*","*","*","*","*","*","*","*",'\n'
+           " 6","*","*","*","*","*","*","*","*",'\n'
+           " 5","*","*","*","*","*","*","*","*",'\n'
+           " 4","*","*","*","*","*","*","*","*",'\n'
+           " 3","*","*","*","*","*","*","*","*",'\n'
+           " 2","*","*","*","*","*","*","*","*",'\n'
+           " 1","*","*","*","*","*","*","*","*",'\n'
+           "  ","1","2","3","4","5","6","7","8","(x)"]
     counter=0
     while counter!=8:
         x=random.randint(1,len(field1)-1)
@@ -176,21 +145,18 @@ def mineSweeper():
             field1[x]="M"
             counter+=1
     counter=0
-    counter1=0
     firstTimeCheck=1
-    while counter!=8 or counter1!=0:
+    while counter!=8 and counter1!=0:
         for i in field:
             if i =="*":
                 counter1+=1
             if i =="F":
                 counter+=1
-        answer=answer1(field)
+        coordinates=input("Type in the X and Y coordinates you want to unlock in the following format\n 'x,y': ")
         field=unlock(coordinates,field1,field,firstTimeCheck)
         firstTimeCheck=0
         if field==-1:
             answer=input("You hit a bomb, wanna go again? (yes/no): ")
-            while answer!="yes" or answer!="Yes" or answer !="no" or answer!="No":
-                answer=input("(Input Error) You hit a bomb, wanna go again? (yes/no): ")
             return answer
         print(*field,*field1)
     answer=1
@@ -199,9 +165,9 @@ def mineSweeper():
 
 print("Welcome to Minesweeper!\nType 'Help' if you want instructions for this game!\nType 'Start' if you want to begin!")
 answer=input("What do you want to do?\n: ")
-while answer == "start" or answer=="Start" or answer=="Start." or answer=="start.":
+while answer == "start":
     answer=mineSweeper()
-    while answer=="yes" or answer=="Yes" or answer=="yes." or answer=="Yes.":
+    if answer=="yes":
         answer=mineSweeper()
     if answer==1:
         print("Thank you for playing!")
